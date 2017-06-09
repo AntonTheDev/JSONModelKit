49dcf66c684a1fc7ab7a159b8b397f495aa5dea4
Anton <anton.doudarev@gmail.com>
2017-06-09 10:36:25 -0400

    Changed import statement for importer

--- Source/ModelScript/fileimporter.py
+++ Source/ModelScript/fileimporter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from pbxproj import XcodeProject
+import pbxproj
 
 class ProjectFileImporter:
 
@@ -13,7 +13,7 @@ class ProjectFileImporter:
    def importmodelfiles():
    		instantiatorPath = output_directory + 'Internal/JMInstantiator.swift'
 		instantiatorGenerator = InstantiatorGenerator(plistPaths, output_directory, version, testEnabled, jsonFormatEnabled)
-	
+
 		generate_file(instantiatorGenerator.internalGeneratedClass(), instantiatorPath, True)
 
         # open the project
@@ -21,15 +21,15 @@ class ProjectFileImporter:
 
 		for mappingPath in plistPaths:
 			classname = mappingPath[mappingPath.rindex('/',0,-1)+1:-1] if mappingPath.endswith('/') else mappingPath[mappingPath.rindex('/')+1:].split('.', 1 )[0]
-		
+
 			classGenerator = ClassGenerator(mappingPath, output_directory, version, testEnabled, jsonFormatEnabled)
-		
+
 			internalClassPath = output_directory + 'Internal/_'+ classname + '.swift'
 			externalClassPath = output_directory + classname + '.swift'
-		
+
 			# add a file to it, force=false to not add it if it's already in the project
 			#project.add_file(internalClassPath, force=False)
 			#project.add_file(externalClassPath, force=False)
 			#save the project, otherwise your changes won't be picked up by Xcode
-		
+
 		#project.save()
