ed70d77e54c62c3125a319a0e68a3510aa3d9bf5
Anton Doudarev <antonthedev@gmail.com>
2017-06-10 00:22:57 -0400

    Added WatchKit test Apps, and test targets

---
+++ JSONModelKit/JSONModelKit-watchOS/JSONModelKit-watchOS WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  JSONModelKit-watchOS WatchKit Extension
+//
+//  Created by Anton on 6/10/17.
+//  Copyright © 2017 Anton Doudarev. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ JSONModelKit/JSONModelKit-watchOS/JSONModelKit-watchOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSONModelKit-watchOS
+//
+//  Created by Anton on 6/10/17.
+//  Copyright © 2017 Anton Doudarev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
